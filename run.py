cb603ae0c1aa819e7dd1813ae698f5e60f40163e
Gabi Kazav <gabika@monday.com>
2024-12-12 19:05:28 +0200

    0.0016

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from app import create_app
 # Create the Flask app instance
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     # Run the app in debug mode for development
     app.run(debug=True, host="0.0.0.0", port=8080)
