7ee5e942f225a8133428883023aa3ec99db3736f
Gabi Kazav <gabika@monday.com>
2024-12-13 22:46:51 +0200

    0.02

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,12 @@ from .models import db
 def create_app():
     app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
+    @app.context_processor
+    def inject_globals():
+        return {
+            'currency_symbol': os.getenv('CURRENCY_SYMBOL', '$')
+        }
+
     # Load configurations
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///waste_tracking.db")
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
